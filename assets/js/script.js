630deb6f209304fe7ce657859ed3a8640b2c64ed
Jime Mellado <mellado.jimena@gmail.com>
2026-02-02 17:46:59 -0300

    Se inicializa proyecto con leccion 1

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Avance lección 1
+/*
+- prompt para pedir al usuario un dato
+- alert: para mostrar un mensaje al usuario
+
+*/
+
+console.log(
+  "%c******* 🧚 Bienvenido a la mejor aplicación en consola del mundo mundial 🧚 *******",
+  "color:white; font-size: 13px; background-color: purple; padding: 5px;",
+);
+
+prompt("Por favor, ingresa tu nombre:");
+
+alert("¡Gracias por usar nuestra aplicación, hasta luego!");
