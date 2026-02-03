09febb0131f961b439d3f2290169f1555df4f2c9
jmellado89 <91641137+jmellado89@users.noreply.github.com>
2026-02-03 15:25:49 -0300

    Revert "Revert "Se añaden operaciones artiméticas de leccion 2""

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,51 @@ console.log(
   "color:white; font-size: 13px; background-color: purple; padding: 5px;",
 );
 
-prompt("Por favor, ingresa tu nombre:");
+const texto = document.getElementById("mostrar");
+let nombre = prompt("Por favor, ingresa tu nombre:");
+let num1 = parseInt(prompt(`Hola ${nombre}, por favor ingresa un número: `));
+let num2 = parseInt(prompt("Ahora ingresa otro número: "));
+let operacion = prompt(
+  "Qué deseas hacer con estos dos números? (Sumar, restar, dividir, multiplicar",
+).toLowerCase();
+let resultado;
 
-alert("¡Gracias por usar nuestra aplicación, hasta luego!");
+calcular();
+
+function calcular() {
+  switch (operacion) {
+    case "sumar":
+      resultado = num1 + num2;
+      console.log(`El resultado de ${num1} + ${num2} es ${resultado}`);
+      texto.textContent = `El resultado de ${num1} + ${num2} es ${resultado}`;
+      break;
+    case "restar":
+      resultado = num1 - num2;
+      console.log(`El resultado de ${num1} - ${num2} es ${resultado}`);
+      texto.textContent = `El resultado de ${num1} - ${num2} es ${resultado}`;
+      break;
+    case "dividir":
+      if (num2 !== 0) {
+        resultado = num1 / num2;
+        console.log(`El resultado de ${num1} / ${num2} es ${resultado}`);
+        texto.textContent = `El resultado de ${num1} / ${num2} es ${resultado}`;
+      } else {
+        console.log("No se puede dividir por 0");
+        texto.textContent = `No se puede dividir por 0`;
+      }
+      break;
+    case "multiplicar":
+      resultado = num1 * num2;
+      console.log(`El resultado de ${num1} * ${num2} es ${resultado}`);
+      texto.textContent = `El resultado de ${num1} * ${num2} es ${resultado}`;
+      break;
+    default:
+      console.log(
+        "Debes ingresar una de las operaciones definidas (sumar, restar, dividir o multiplicar)",
+      );
+      texto.textContent =
+        "Debes ingresar una de las operaciones definidas (sumar, restar, dividir o multiplicar)";
+  }
+}
+
+// alert("¡Gracias por usar nuestra aplicación, hasta luego!");
