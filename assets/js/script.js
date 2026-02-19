464407acdf754552c7cd818e826d984de12284b7
Jime Mellado <mellado.jimena@gmail.com>
2026-02-19 13:28:02 -0300

    Se añade nuevo objeto con método y otros ejercicios

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,16 @@ console.log(
   "color:white; font-size: 13px; background-color: purple; padding: 5px;",
 );
 
-const texto = document.getElementById("mostrar");
-let nombre = prompt("Por favor, ingresa tu nombre:");
-let num1 = parseInt(prompt(`Hola ${nombre}, por favor ingresa un número: `));
-let num2 = parseInt(prompt("Ahora ingresa otro número: "));
-let operacion = prompt(
-  "Qué deseas hacer con estos dos números? (Sumar, restar, dividir, multiplicar",
-).toLowerCase();
-let resultado;
+// const texto = document.getElementById("mostrar");
+// let nombre = prompt("Por favor, ingresa tu nombre:");
+// let num1 = parseInt(prompt(`Hola ${nombre}, por favor ingresa un número: `));
+// let num2 = parseInt(prompt("Ahora ingresa otro número: "));
+// let operacion = prompt(
+//   "Qué deseas hacer con estos dos números? (Sumar, restar, dividir, multiplicar",
+// ).toLowerCase();
+// let resultado;
 
-calcular();
+// calcular();
 
 function calcular() {
   switch (operacion) {
@@ -77,7 +77,7 @@ function multiplicar(a, b) {
   return a * b;
 }
 
-//Lección 3
+//Lección 3 y 5
 // Artistas de Lollapalooza que me gustan
 
 const artistas = [
@@ -108,15 +108,58 @@ while (j < artistas.length) {
   }
   j++;
 }
-console.log(`La cantidad de artistas masculinos es: ${cantMasc}`);
+console.log(`\nLa cantidad de artistas masculinos es: ${cantMasc}`);
 
 // Cantidad de artistas que tocan el Domingo
 let artistasDom = artistas.filter((artista) => artista.dia === "domingo");
 console.log(
-  `La cantidad de artistas que tocan el Domingo son: ${artistasDom.length}`,
+  `\nLa cantidad de artistas que tocan el Domingo son: ${artistasDom.length}`,
 );
 
 // alert("¡Gracias por usar nuestra aplicación, hasta luego!");
 
-// Cómo saber si un elemento es un array o un objeto?
-console.log(Array.isArray(artistas)); // true
+// Leccion 5: Crear objeto con métodos
+
+const artistaFavorito = {
+  nombre: "Sabrina",
+  apellido: "Carpenter",
+  genero: "femenino",
+  generoMusical: "pop",
+  cancionFavorita: "Manchild",
+  cantidadFans: 10000000,
+
+  mostrarInfo() {
+    return `\nMi artista favorito es ${this.nombre} ${this.apellido}, su género musical es ${this.generoMusical} y mi canción favorita es ${this.cancionFavorita}.`;
+  },
+
+  sumarFans(nuevosFans) {
+    this.cantidadFans += nuevosFans;
+    return `\nSe han sumado ${nuevosFans.toLocaleString("es-ES")} fans. Ahora ${this.nombre} tiene ${this.cantidadFans.toLocaleString("es-ES")} fans.`;
+  },
+};
+
+console.log(artistaFavorito.mostrarInfo());
+console.log(artistaFavorito.sumarFans(500000));
+
+console.log("\nLista de artistas del día Domingo:");
+artistasDom.forEach((artista) => console.log(`- ${artista.nombre}`));
+
+const artistas26 = artistas.map((artista) => {
+  artista.festival = "Lollapalooza";
+  artista.year = 2026;
+  return artista.nombre;
+});
+
+console.log("\nMejores artistas de Lollapalooza 2026:");
+artistas26.forEach((artista, indice) => {
+  console.log(`${indice + 1}. ${artista}`);
+});
+
+artistas.push(
+  { nombre: "Florence and the Machine", festival: "Lollapalooza", year: 2016 },
+  { nombre: "Sam Smith", festival: "Lollapalooza", year: 2019 },
+);
+
+const artistasPast = artistas.filter((artista) => artista.year !== 2026);
+console.log("\nAlgunos de los mejores artista de otros años:");
+artistasPast.forEach((artista) => console.log(`- ${artista.nombre}`));
